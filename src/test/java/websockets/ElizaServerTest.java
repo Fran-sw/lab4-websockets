f72f788ab43f4c95cedaecc8c8135e58f86e8d48
Fran-sw <franmabad@gmail.com>
2020-11-20 16:47:28 +0100

    Completed Test

--- src/test/java/websockets/ElizaServerTest.java
+++ src/test/java/websockets/ElizaServerTest.java
@@ -56,16 +56,23 @@ public class ElizaServerTest {
 	}
 
 	@Test(timeout = 1000)
-	@Ignore
 	public void onChat() throws DeploymentException, IOException, URISyntaxException, InterruptedException {
-		// COMPLETE ME!!
 		List<String> list = new ArrayList<>();
 		ClientEndpointConfig configuration = ClientEndpointConfig.Builder.create().build();
 		ClientManager client = ClientManager.createClient();
-		client.connectToServer(new ElizaEndpointToComplete(list), configuration, new URI("ws://localhost:8025/websockets/eliza"));
-		// COMPLETE ME!!
-		// COMPLETE ME!!
-		// COMPLETE ME!!
+		Session session = client.connectToServer(new ElizaEndpointToComplete(list), configuration, new URI("ws://localhost:8025/websockets/eliza"));
+		
+		session.getAsyncRemote().sendText("hello doc!");	// We start the conversation
+		Thread.sleep(50);									//Wait for the response
+
+		assertEquals("The doctor is in.",list.get(0));		//Check the response is okey and we can keep chatting
+		assertEquals("What's on your mind?",list.get(1));
+
+		//we can chat with Eliza
+
+		session.getAsyncRemote().sendText("bye");			// We close the chat
+		Thread.sleep(50);									//Wait for the response
+		assertEquals(5,list.size());						// Check it has replied to us
 	}
 
 	@After
@@ -102,8 +109,6 @@ public class ElizaServerTest {
         @Override
         public void onOpen(Session session, EndpointConfig config) {
 
-            // COMPLETE ME!!!
-
             session.addMessageHandler(new ElizaMessageHandlerToComplete());
         }
 
@@ -112,7 +117,6 @@ public class ElizaServerTest {
             @Override
             public void onMessage(String message) {
                 list.add(message);
-                // COMPLETE ME!!!
             }
         }
     }
